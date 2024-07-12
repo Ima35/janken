2eaa341d280e37d30806eefc49f6569ea9bdb9e5
Imairi <m.imairi.sys23@morijyobi.ac.jp>
2024-07-12 12:29:17 +0900

    CPUの手をランダムに設定した close #15, close #16

--- src/Main.java
+++ src/Main.java
@@ -42,7 +42,12 @@ public class Main {
             System.exit(0);
             return;
         }
-        //以降にテストコードを追加して検証を行う
-        System.out.println(user.getHand().getHandName());
+        //comの手を生成
+        com.createRandomHand();
+//以降にテストコードを追加して検証を行う
+
+        System.out.println("ランダムなジャンケンの手をCPUにセットする");
+
+        System.out.println(com.getHand().getHandName());
     }
 }
